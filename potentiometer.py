b8f31dc2e8642292494343bfc4bf334c17d60c4d
SSCGS2026 <77761@cgs.act.edu.au>
2026-08-27 08:43:33 +1000

    Update potentiometer.py
    made instructions clearer

--- potentiometer.py
+++ potentiometer.py
@@ -7,3 +7,5 @@ while True:
     PotentiometerValue = Potentiometer.read_u16()
     print(PotentiometerValue)
     sleep(0.1)
+#You should be able to just use the "potentiometervalue" for use in other things, and you could also divide by 65535 and times by 100 to get an
+#Aproximate percentage value
