cfc37fb64494e87448d76ec0e8fab0418950cc86
SSCGS2026 <77761@cgs.act.edu.au>
2026-08-26 15:25:20 +1000

    Update buzzer+LED.py

--- buzzer+LED.py
+++ buzzer+LED.py
@@ -1,13 +1,14 @@
 from machine import Pin, PWM
 from utime import sleep
 
-buzzer = PWM(Pin(15))
+buzzer = PWM(Pin(1))
 buzzer.freq(500)
 led = Pin(0, Pin.OUT)
 
 while True:
     led.toggle()
+    buzzer.duty_u16(50000)
     sleep(0.5)
-    buzzer.duty_u16(1000)
-    sleep(1)
     buzzer.duty_u16(0)
+    led.toggle()
+    sleep(0.5)
