ebb267ed8c762c008e734c91944ed756263a07ef
SSCGS2026 <77761@cgs.act.edu.au>
2026-08-27 08:27:35 +1000

    Add files via upload
    This is a thing I did at home, it should work with our multicoloured LED, and I gave instructions in the comments on how to change the colour

---
+++ MultiColorLED.py
@@ -0,0 +1,21 @@
+from machine import Pin, PWM
+
+freq_num = 10000
+
+redLED = PWM(Pin(0))
+greenLED = PWM(Pin(1))
+blueLED = PWM(Pin(2))
+redLED.freq(freq_num)
+greenLED.freq(freq_num)
+blueLED.freq(freq_num)
+
+def setColor(r, g, b):
+    redLED.duty_u16(r)
+    greenLED.duty_u16(g)
+    blueLED.duty_u16(b)
+    
+red   = 0   #Choose number between 0 and 65535
+green = 255 #Choose number between 0 and 65535
+blue  = 255 #Choose number between 0 and 65535
+#Because it's a little unclear how this works, use this website to pick colours: https://www.w3schools.com/colors/colors_picker.asp, it worked for me when I did it at home
+setColor(red, green, blue)
