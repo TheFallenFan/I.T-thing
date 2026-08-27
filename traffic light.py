d0a20af9ffa203bc289f941a2e2ce297252deefd
SSCGS2026 <77761@cgs.act.edu.au>
2026-08-27 11:42:55 +1000

    Added a clear example for the RGBLED

---
+++ traffic light.py
@@ -0,0 +1,36 @@
+from machine import Pin, PWM
+from time import sleep
+
+freq_num = 10000
+
+redLED = PWM(Pin(0))
+greenLED = PWM(Pin(1))
+blueLED = PWM(Pin(2))
+redLED.freq(freq_num)
+greenLED.freq(freq_num)
+blueLED.freq(freq_num)
+
+def set_led(r, g, b):
+    red = r*257
+    green = g*257
+    blue = b*257
+    return red, green, blue
+
+def change_LED(red, green, blue):
+    redLED.duty_u16(red)
+    greenLED.duty_u16(green)
+    blueLED.duty_u16(blue)
+
+while True:
+    change_LED(*set_led(255, 0, 0))
+    sleep(3)
+    for i in range (0, 5):
+        change_LED(*set_led(205, 93, 52))
+        sleep(0.5)
+        change_LED(*set_led(0, 0, 0))
+        sleep(0.5)
+    change_LED(*set_led(0, 255, 0))
+    sleep(3)
+
+
+#You should be able to change triggers easily for this one
