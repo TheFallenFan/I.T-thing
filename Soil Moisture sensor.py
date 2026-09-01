15ab5fef629ca87ba75c5dda8c21d64e47ac376d
Toby341 <toby.reiner@cgs.act.edu.au>
2026-09-02 09:24:50 +1000

    Refactor soil moisture sensor implementation
    Refactor soil moisture sensor code to improve readability and functionality.

--- Soil Moisture sensor.py
+++ Soil Moisture sensor.py
@@ -1,7 +1,16 @@
-from import machine import Pin, ADC
-import dht
-import time 
+              from machine import ADC, Pin
+import time
 
-soil_sensor = ADC(26)
-potentiometer = ADC(27)
-dht_sensor = dht.DHT11(
+s1 = ADC(Pin(26))
+s2 = ADC(Pin(27))
+l = Pin(15, Pin.OUT)
+
+while True:
+    avg_moisture = (s1.read_u16() + s2.read_u16()) / 2
+    
+    if avg_moisture > 45000:
+        l.on()
+        time.sleep(30)
+        l.off()
+        
+    time.sleep(1)
