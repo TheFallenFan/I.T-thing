ea88a960baf1b18d1f5bacc1d2d0eac08f434818
SSCGS2026 <77761@cgs.act.edu.au>
2026-09-02 09:27:04 +1000

    With help from Toby

---
+++ Humidity+Temp.py
@@ -0,0 +1,12 @@
+import machine
+import dht
+import time
+
+sensor = dht.DHT11(machine.Pin(16))
+
+while True:
+
+    sensor.measure()
+    temp = str(sensor.temperature()) + "°C"
+    hum = str(sensor.humidity()) + "%"
+    print("Temperature:", temp, "| Humidity:" ,hum)
